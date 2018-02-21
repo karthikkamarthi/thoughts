7cbba426659da75a68618bccf9b0073eb19bf7b9
karthik kamarthi <karthik.k6071@gmail.com>
2018-02-22 00:40:07 +0530

    'ready'

--- server.js
+++ server.js
@@ -14,14 +14,15 @@ var http = require('http')
   , io = require('socket.io').listen(server);
 
 
-var jade = require('jade');
+
 // var io = require('socket.io').listen(app);
 var pseudoArray = ['admin']; //block the admin username (you can disable it)
 
 // Views Options
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
 app.set("view options", { layout: false });
 
 app.use(express.static(__dirname + '/public'));
@@ -29,7 +30,7 @@ app.use(express.static(__dirname + '/public'));
 // Render and send the main page
 
 app.get('/', function(req, res){
-  res.render('home.jade');
+  res.render('home.html');
 });
 server.listen(appPort,ip);
 // app.listen(appPort);
@@ -40,49 +41,25 @@ console.log("Server listening on port " + appPort);
 var users = 0; //count the users
 
 io.sockets.on('connection', function (socket) { // First connection
-	users += 1; // Add 1 to the count
-	reloadUsers(); // Send the count to all the users
-	socket.on('message', function (data) { // Broadcast the message to all
-		if(pseudoSet(socket))
-		{
-			var transmit = {date : new Date().toISOString(), pseudo : socket.nickname, message : data};
-			socket.broadcast.emit('message', transmit);
-			console.log("user "+ transmit['pseudo'] +" said \""+data+"\"");
+
+
+	socket.on('joinRoom', function (data) { // Broadcast the message to all
+		if (data === "rite") {
+			socket.room= "meshup";
+			socket.join("meshup");
+			var transmit = {message : "sender has joined"};
+			socket.broadcast.to(socket.room).emit('message', transmit);
 		}
-	});
-	socket.on('setPseudo', function (data) { // Assign a name to the user
-		if (pseudoArray.indexOf(data) == -1) // Test if the name is already taken
-		{
-			pseudoArray.push(data);
-			socket.nickname = data;
-			socket.emit('pseudoStatus', 'ok');
-			console.log("user " + data + " connected");
-		}
-		else
-		{
-			socket.emit('pseudoStatus', 'error') // Send the error
-		}
-	});
-	socket.on('disconnect', function () { // Disconnection of the client
-		users -= 1;
-		reloadUsers();
-		if (pseudoSet(socket))
-		{
-			console.log("disconnect...");
-			var pseudo;
-			pseudo = socket.nickname;
-			var index = pseudoArray.indexOf(pseudo);
-			pseudo.slice(index - 1, 1);
-		}
-	});
 });
 
-function reloadUsers() { // Send the count of the users to all
-	io.sockets.emit('nbUsers', {"nb": users});
-}
-function pseudoSet(socket) { // Test if the user has a name
-	var test;
-	if (socket.nickname == null ) test = false;
-	else test = true;
-	return test;
-}
+	socket.on('message', function (data) { // Broadcast the message to all
+	     if(socket.room) {
+			var transmit = {message : data};
+			socket.broadcast.to(socket.room).emit('message', transmit);
+
+		 }
+	});
+
+	socket.on('disconnect', function () { // Disconnection of the client
+	});
+});
